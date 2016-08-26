931cb48f9f7b102d65f4d8afb3a32f35919abe9a
lsjgerardo <lsjgerardo@gmail.com>
2016-08-26 16:13:37 -0500

    comps

---
+++ p2/heap_comps.py
@@ -0,0 +1,86 @@
+comps = 0
+def heapify(list,i):
+    
+
+    global comps
+
+    if(2*i + 1 	<= len(list)-1 ):
+		comps+=1
+		if(list[2*i] < list[2*i + 1]):
+			comps+=1
+			max = 2*i+1
+		else:
+			max = 2*i
+		
+		if (list[i] < list[max]):
+			comps+=1
+			aux		 = list[i]
+			list[i]	 = list[max]
+			list[max] = aux
+
+	elif (2*i <= len(list)-1):
+		comps+=1
+		if(2*i <= len(list)-1):
+			comps+=1
+			aux		 = list[i]
+			list[i]	 = list[2*i]
+			list[2*i] = aux
+	
+	return list
+
+
+def heapify_rec(list,i):
+    global comps
+	if(2*i + 1 	<= len(list)-1 ):
+		comps+=1
+		if(list[2*i] < list[2*i + 1]):
+			comps+=1
+			max = 2*i+1
+		else:
+			max = 2*i
+
+		if (list[i] < list[max]):
+			comps+=1
+			aux		 = list[i]
+			list[i]	 = list[max]
+			list[max] = aux
+			heapify_rec(list,max)
+
+	elif (2*i <= len(list)-1):
+		comps+=1
+		if(list[i] < list[2*i]):
+			comps+=1
+			aux		 = list[i]
+			list[i]	 = list[2*i]
+			list[2*i] = aux
+			heapify_rec(list,2*i)
+	
+	return list
+
+
+
+
+def heap(list):
+    global comps
+	for i in range(len(list)//2,1,-1 ):
+		list = heapify(list,i)
+	list =	heapify_rec(list,1 )
+	list3 = []
+	for i in range(1, len(list) ):
+		aux			 = list[1]
+		list[1] 	 = list[len(list)-1]
+		list[len(list)-1] = aux
+		list3.append(aux)
+		list = list[:len(list)-1]
+		heapify_rec(list,1)
+	return list3
+
+#print( heapify([10,2,3,11],1) )
+print ( heap([2,1,6,8,0,88,451,254]) )  #Ordena de MAYOR a MENOR lol
+print(comps)
+print("------------------")
+print ( heap([2,1,6,8,-5,88,-451,254]) )  #Ordena de MAYOR a MENOR lol
+print(comps)
+	
+
+

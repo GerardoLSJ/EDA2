302267cb27ad3c590b9dfbb7f798e481c5ad866e
lsjgerardo <lsjgerardo@gmail.com>
2016-08-26 17:01:54 -0500

    quick comps

--- p2/quick.py
+++ p2/quick.py
@@ -1,36 +1,54 @@
-def reacomodo(lista,posPivote):
-	j=0
-	for i in range(0,posPivote):
-		if (lista[i] < lista[posPivote]):
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#Import library for stdin 
+import sys
+#import numpy as np
+import time
 
-			aux		 = lista[j]
-			lista[j] = lista[i]
-			lista[i] = aux
-			j+=1
-	
-	aux		 = lista[j]
-	lista[j] = lista[posPivote]
-	lista[posPivote] = aux
-	return [lista, j] 
+comps = 0
+def reacomodo(lista,posPivote):
+    global comps
+    j=0
+    for i in range(0,posPivote):
+        if (lista[i] < lista[posPivote]):
+            comps+=1
+
+            aux		 = lista[j]
+            lista[j] = lista[i]
+            lista[i] = aux
+            j+=1
+    
+    aux		 = lista[j]
+    lista[j] = lista[posPivote]
+    lista[posPivote] = aux
+    return [lista, j] 
 
 
 
 
 
 def quick(lista):
-	if(len(lista) <= 1):
-		return lista
-	
-	pivote	= len(lista)-1
-	retorno = reacomodo(lista,pivote)
-	lista 	= retorno[0]
-	piv 	= retorno[1]
+    global comps
+    if(len(lista) <= 1):
+        comps+=1
+        return lista
+    
+    pivote	= len(lista)-1
+    retorno = reacomodo(lista,pivote)
+    lista 	= retorno[0]
+    piv 	= retorno[1]
 
-	lista1	= quick(lista[:piv])
-	lista2  = quick(lista[piv+1:])
-	lista1.append(lista[piv])
-	return lista1[:] + lista2[:]
+    lista1	= quick(lista[:piv])
+    lista2  = quick(lista[piv+1:])
+    lista1.append(lista[piv])
+    return lista1[:] + lista2[:]
 
 
-print( quick(['a',4,6,2,1,3,8,5,5,8,4,15152,4,847,-514,5,'b']) )
-print( quick(['a','A','b','z' ]) )
+for line in sys.stdin:
+    line = line.replace('[','')
+    line = line.replace(']','')
+    line = line.split(',')
+    line = list(map(int, line))
+    comps = 0
+    print (quick(line))
+    print(comps)

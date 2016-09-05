3b60403bdafe01eaef83a1d7402cc06942e041a1
gerardolsj <lsjgerardo@gmail.com>
2016-09-04 19:29:44 -0500

    comment functions

--- p4/letterRadix.py
+++ p4/letterRadix.py
@@ -3,27 +3,27 @@ def radix(l):
     for i in l:
         if(len(i) > max ):
             max = len(i)
-    for i in range(len(l)):                         #normalizar la cadena ceros en decimas centenas etc
+    for i in range(len(l)):                         #normalizar
         while(len(l[i]) < max):
-            l[i] = l[i]+chr(96)                     #123  #Normalizar por la derecha!
-
-    for j in range(max-1,-1,-1):                    #max-1 hasta -1 en incrementos de -1
-        ocu = [ 0 for i in range(0,28) ]
+            l[i] = l[i]+chr(96)                     #Normalizar por la derecha
+                                                    #coon el char 96 
+    for j in range(max-1,-1,-1):                    
+        ocu = [ 0 for i in range(0,28) ]        # arr de 28 por el alfabeto
 
 
         for i in range(len(l)):
 
-            ocu[ int(ord(l[i][j])-96 ) ] +=1
-
+            ocu[ int(ord(l[i][j])-96 ) ] +=1    #para ocupar la localidad 1
+                                                # con la 'a' le restamos 96
           					
         
-        for i in range(0,27):
+        for i in range(0,27):                   #suma de parciales
             ocu[i+1] = ocu[i] + ocu[i+1]
 
         
         s = [0 for i in range(len(l))]
         
-        for i in range(len(l)-1,-1,-1):
+        for i in range(len(l)-1,-1,-1):         #acomodo en arreglo parcial
             ocu[ int(ord(l[i][j]) -96) ] -=1
             s[ ocu[ int(ord(l[i][j]) -96) ] ] = l[i]
         l=s
@@ -35,5 +35,5 @@ def radix(l):
 test1 = ['do','l','r','r','s','wf','hf','sy','fj','wr' ]
 
 arr = radix(test1)
-arr = list(map(lambda x: x.replace(chr(96),"") , arr))   #Borramos las " chr(96) " que agregamos para normalizar
-print(arr)
+arr = list(map(lambda x: x.replace(chr(96),"") , arr))  #Borramos las "chr(96) "
+print(arr)                                              #que agregamos para normalizar
--- p4/radix.py
+++ p4/radix.py
@@ -5,23 +5,24 @@ def radix(l):
     for i in l:
         if(len(i) > max ):
             max = len(i)
-    for i in range(len(l)):   #normalizar la cadena ceros en decimas centenas etc
+    for i in range(len(l)):             #normalizar la cadena con ceros 
         while(len(l[i]) < max):
             l[i] = "0"+l[i]
 
     
 
-    for j in range(max-1,-1,-1): #max-1 hasta -1 en incrementos de -1
-        ocu = [ 0 for i in range(10) ]
-        for i in range(len(l)):
-            ocu[ int(l[i][j]) ] +=1					#cast y ...accedemos a "123" luego al "3"
+    for j in range(max-1,-1,-1):         
+        ocu = [ 0 for i in range(10) ] #arr de ocurr por "decena" "centena" etc.
+        for i in range(len(l)):         
+            ocu[ int(l[i][j]) ] +=1		 #cast, accedemos a "123" luego al "3"
+                                         #Con [][j] el segundo argumento
+
+        for i in range(9):               # sumamos las ocurrencias 
+            ocu[i+1] = ocu[i] + ocu[i+1] 
         
-        for i in range(9):
-            ocu[i+1] = ocu[i] + ocu[i+1]
+        s = [0 for i in range(len(l))]  #inicializamos arreglo vacio de 0s
         
-        s = [0 for i in range(len(l))]
-        
-        for i in range(len(l)-1,-1,-1):
+        for i in range(len(l)-1,-1,-1): #Acomodams en la posición que debes
             ocu[ int(l[i][j]) ] -=1
             s[ ocu[ int(l[i][j]) ] ] = l[i]
         l=s

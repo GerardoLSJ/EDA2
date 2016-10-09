d992f11bf8c047403914ffff214070c42694f099
gerardolsj <lsjgerardo@gmail.com>
2016-10-09 18:45:55 -0500

    comment

--- p8/treeHR.py
+++ p8/treeHR.py
@@ -6,34 +6,34 @@ for line in sys.stdin:
 arb = arr[0].replace("[","").replace("]","").replace("\n","").split(",")
 inser = arr[1].replace("[","").replace("]","").replace("\n","").split(",")
 remove = arr[2].replace("[","").replace("]","")
+#Normalizar la cadena de texto
 
 arb = list(map(int,arb))
 inser = list(map(int,inser))
-#print(arb)
-#print(inser)
-#print(remove)
-
+#cast a los elementos
 
 def insertar(v,p): #valor(o nodo), posicion
-	if p == len(arb):
-		arb.append(v)
+	if p == len(arb): #Si va al final de la lista
+		arb.append(v) #se inserta y ya
 		
 	elif p < len(arb):
 		if hijos(p) == 1 or hijos(p) ==3:
-			#tien izq o ambos
-			aux = arb[p]
-			arb[p] = v
+			#tiene hijos izq o ambos
+			aux = arb[p] #elemento que se dezplazara
+			arb[p] = v	 #elemento insertado
 			insertar(aux, 2*p)
-			
+			#ahora se inseerta el elemento dezplazado
+			#de manera recursiva hasta que quede en una 
+			#hoja un nodo dezplazado y solo entre al primer IF
+			# o el ultimo ELIF
 		elif(hijos(p) == 2):
 			#solo tiene hijo derecho
-			arb[2*p] = arb[p]
-			arb[p] = v
-			#insertar(aux,2*p+1)
+			arb[2*p] = arb[p] #Ahora tiene hijo izquierdo :)
+			arb[p] = v		  # y no tuvimos que dezplazar nada
 		elif hijos(p) == 0:
-			crecer(2*p)
-			arb[2*p] = arb[p]
-			arb[p] = v
+			crecer(2*p)			#Para evitar salirnos de rango
+			arb[2*p] = arb[p]	#pasa al lugar de un hijo izquierdo
+			arb[p] = v			#y el insertado a ser el padre.
 			
 
 
@@ -61,32 +61,36 @@ def hijos(p):
 
 
 	
-def crecer(i):
-	j = i - len(arb)+1
-	for l in range(j):
-		arb.append(None)
+def crecer(i):			#i = 2*p, seria hijo izquierdo
+	j = i - len(arb)+1	#Solo incrementamos la casillas necesarias 
+	for l in range(j):	#para que len(arb) == 2*p y así
+		arb.append(None)	#tener lugar para el hijo izquierdo
 
 
 def decrecer():
 	global arb
 	while(arb[len(arb) -1] == None):
 		arb = arb[:len(arb)-1]
-
+#Recorremos el arreglo de derecha a izquierda
+#eliminando los "None" hasta llegaar a un valor
+#de esa manera no tenemos NONE de más
 
 
 def eliminar(p):
 	if (hijos(p) !=-1 ):
-		if(hijos(p) == 0):
+		if(hijos(p) == 0):	#Si no hay hijos solo lo eliminamos sin más
 			arb[p] = None 
 		elif(hijos(p) == 1  or hijos(p) ==3 ):
-			arb[p] = arb[2*p]
-			eliminar(2*p)
+			#Si tiene hijo izq o ambos
+			arb[p] = arb[2*p] #izquiero pasa a ser padre
+			eliminar(2*p)	  #eliminamos a ese izquierdo
 		
 		elif(hijos(p) == 2 ):
-			arb[p] = arb[2*p+1]
-			eliminar(2*p+1)
+			#Si solo hay hijo derecho
+			arb[p] = arb[2*p+1] #Ese hijo pasa a ser padre
+			eliminar(2*p+1)		#y lo eliminamos 
 			
-	decrecer()
+	decrecer()	#cuando termine la reasignacion debe limpiar los None
 
 
 

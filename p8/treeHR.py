6092764cd37daa3854b52b299814e1f4e69e3fe9
lsjgerardo <lsjgerardo@gmail.com>
2016-10-09 11:02:50 -0500

    pass 50/50

---
+++ p8/treeHR.py
@@ -0,0 +1,106 @@
+import sys
+arr = []
+for line in sys.stdin:
+    arr.append(line)
+
+arb = arr[0].replace("[","").replace("]","").replace("\n","").split(",")
+inser = arr[1].replace("[","").replace("]","").replace("\n","").split(",")
+remove = arr[2].replace("[","").replace("]","")
+
+arb = list(map(int,arb))
+inser = list(map(int,inser))
+#print(arb)
+#print(inser)
+#print(remove)
+
+
+def insertar(v,p): #valor(o nodo), posicion
+	if p == len(arb):
+		arb.append(v)
+		
+	elif p < len(arb):
+		if hijos(p) == 1 or hijos(p) ==3:
+			#tien izq o ambos
+			aux = arb[p]
+			arb[p] = v
+			insertar(aux, 2*p)
+			
+		elif(hijos(p) == 2):
+			#solo tiene hijo derecho
+			arb[2*p] = arb[p]
+			arb[p] = v
+			#insertar(aux,2*p+1)
+		elif hijos(p) == 0:
+			crecer(2*p)
+			arb[2*p] = arb[p]
+			arb[p] = v
+			
+
+
+
+def hijos(p):
+	if ( p <= len(arb)-1 ):	#no existe, fuera de rango
+		
+		if len(arb)-1 >= (2*p+1): #puede que tenga ambos hijos
+			if arb[2*p] != None:  #si tiene hijo izuqierdo
+				if arb[2*p+1] != None: #si tiene ambos hijos
+					return 3
+				return 1 #solo tiene hijo izquierdo
+			elif arb[2*p +1] != None: #Solo tiene hijo derecho
+				return 2
+		elif len(arb)-1 >= 2*p:	#solo hijo izquierdo
+			if arb[2*p] != None:
+				#tiene hijo derecho
+				return 1
+		else:
+			#no tiene hijos
+			return 0
+	else:
+		#no existe
+		return -1
+
+
+	
+def crecer(i):
+	j = i - len(arb)+1
+	for l in range(j):
+		arb.append(None)
+
+
+def decrecer():
+	global arb
+	while(arb[len(arb) -1] == None):
+		arb = arb[:len(arb)-1]
+
+
+
+def eliminar(p):
+	if (hijos(p) !=-1 ):
+		if(hijos(p) == 0):
+			arb[p] = None 
+		elif(hijos(p) == 1  or hijos(p) ==3 ):
+			arb[p] = arb[2*p]
+			eliminar(2*p)
+		
+		elif(hijos(p) == 2 ):
+			arb[p] = arb[2*p+1]
+			eliminar(2*p+1)
+			
+	decrecer()
+
+
+
+
+insertar(inser[0],inser[1])
+eliminar(int(remove))
+print(arb)
+
+
+"""
+Sample Output
+
+[0, 20, 1, 88, 46, 114, None, 40, None, None, 6]
+[0, 20, 1, 88, 46, 114, None, 40, None, None, 6]
+
+
+"""

117646d7b769e7ba27c56ebd0e974210cf603263
lsjgerardo <lsjgerardo@gmail.com>
2016-10-19 09:31:10 -0500

    p10

---
+++ p10/arch.py
@@ -0,0 +1,11 @@
+
+
+
+try:
+    a = open("/users/edaII08/edaII08alu23/Escritorio/EDA2/p10/666hola.txt", "r")
+    lista = a.readlines()
+    print (lista)
+except Exception as e:
+    print(e)
+
+#/users/edaII08/edaII08alu23/Escritorio/EDA2/p10

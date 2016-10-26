138efb417790c86c4d46a347cace1df9a7c65401
LOPEZ SANTIBANEZ JIMENEZ LUIS GERARDO <edaII08alu23@croacia30.red.local>
2016-10-26 09:48:48 -0500

    tidy

--- p11/hola.c
+++ p11/hola.c
@@ -5,14 +5,20 @@
 
 int main() {
   int i;
-  #pragma omp parallel num_threads(5)
+  #pragma omp parallel num_threads(5) shared(i)
   {
     int id = omp_get_thread_num();
-    printf("Hola mundo\n");
-    for ( i = 0; i < 10; i++) {
+    i = id;
+    printf("La var fue mod por %d, nuevo i: %d\n",id,i );
+    /*for ( i = 0; i < 10; i++) {
       printf("Al Thread %d, numero: %d\n",id,i );
     }
+    */
 
   }
+  printf("\n");
+  printf("Fin de EJECUACION\n");
+  printf("varlor final de i: %d\n",i );
+  printf("\n");
 return 0;
 }

9abdfe41e3b046b5f5f5998d529b070eaa0466dc
LOPEZ SANTIBANEZ JIMENEZ LUIS GERARDO <edaII08alu23@croacia30.red.local>
2016-10-26 09:36:44 -0500

    parallel

--- p11/hola.c
+++ p11/hola.c
@@ -1,14 +1,18 @@
+// gcc -fopenmp hola.c -o hola
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
 
 int main() {
   int i;
-  #pragma omp parallel{
+  #pragma omp parallel num_threads(5)
+  {
+    int id = omp_get_thread_num();
+    printf("Hola mundo\n");
     for ( i = 0; i < 10; i++) {
-      printf("Hola mundo\n");
+      printf("Al Thread %d, numero: %d\n",id,i );
     }
-    return 0;
-  }
 
+  }
+return 0;
 }
